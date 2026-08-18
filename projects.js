9430c2663220d1dd2986404d19abf02f6980efdb
Glen Sasek <sasekg@gmail.com>
2026-08-17 18:56:09 -0700

    Add pool chem project link

--- projects.js
+++ projects.js
@@ -1,26 +1,30 @@
 (() => {
   navigationStrings = {
-    prompt: 'rootpdx:~$',
-    terminalLabel: 'Open a numbered section',
-    navigationLabel: 'ROOTPDX sections',
-    promptHelp: 'Enter a number to open that item.',
+    prompt: "rootpdx:~$",
+    terminalLabel: "Open a numbered section",
+    navigationLabel: "ROOTPDX sections",
+    promptHelp: "Enter a number to open that item.",
     navigation: [
       {
-        label: '[1] This website: github.com/sasekg/rootpdx',
-        href: 'https://github.com/sasekg/rootpdx'
+        label: "[1] This website: github.com/sasekg/rootpdx",
+        href: "https://github.com/sasekg/rootpdx",
       },
       {
-        label: '[2] Virtual Greeter https://github.com/sasekg/Get-To-Know-Glen-Sasek',
-        href: 'https://github.com/sasekg/Get-To-Know-Glen-Sasek'
+        label: "[2] Pool Chemical Calculator: github.com/sasekg/Pool-Chemical-Calculator",
+        href: "https://github.com/sasekg/rootpdx/Pool-Chemical-Calculator/index.html",
       },
       {
-        label: '[3] https://github.com/sasekg/Pool-Chemical-Calculator',
-        href: 'https://github.com/sasekg/Pool-Chemical-Calculator'
+        label: "[3] Virtual Greeter https://github.com/sasekg/Get-To-Know-Glen-Sasek",
+        href: "https://github.com/sasekg/Get-To-Know-Glen-Sasek",
       },
       {
-        label: '[4] https://github.com/sasekg/Acoustic-Pick-Rescue-Challenge',
-        href: 'https://github.com/sasekg/Acoustic-Pick-Rescue-Challenge'
-      }
-    ]
+        label: "[4] https://github.com/sasekg/Pool-Chemical-Calculator",
+        href: "https://github.com/sasekg/Pool-Chemical-Calculator",
+      },
+      {
+        label: "[5] https://github.com/sasekg/Acoustic-Pick-Rescue-Challenge",
+        href: "https://github.com/sasekg/Acoustic-Pick-Rescue-Challenge",
+      },
+    ],
   };
 })();
